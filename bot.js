da7c1d7ca3fd344ecc56e092dc5b70afbb569234
martinenti <40662211+martinenti@users.noreply.github.com>
2018-08-22 20:12:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -88,7 +88,22 @@ message.author.send(`**مدة الرابط : يـوم
   
 
 
+ client.on('message', message => { 
+    var prefix = "+";
+ let args = message.content.split(' ').slice(1);
+    if(message.content.startsWith(prefix + 'اختصر')) {
+    if(!message.channel.guild) return;  
 
+        googl.setKey('AIzaSyC2Z2mZ_nZTcSvh3QvIyrmOIFP6Ra6co6w');
+        googl.getKey();
+        googl.shorten(args.join(' ')).then(shorturl => {
+            message.channel.send(''+shorturl)
+        }).catch(e=>{
+            console.log(e.message);
+            message.channel.send('خطأ!');
+        });
+}
+});
 
 
 
