213935581faefd1cac0c19cc21f3c2be18ad2f8b
martinenti <40662211+martinenti@users.noreply.github.com>
2018-08-22 20:14:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -106,7 +106,20 @@ message.author.send(`**مدة الرابط : يـوم
 });
 
 
-
+client.on('guildCreate', guild => {
+client.channels.get("ROOM ID").send(:white_check_mark: **${client.user.tag} دخل سيرفر جديد
+Server name: __${guild.name}__
+Server owner: __${guild.owner}__
+Server id: __${guild.id}__ 
+Server Count: __${guild.memberCount}__**)
+});
+client.on('guildDelete', guild => {
+  client.channels.get("ROOM ID").send(:negative_squared_cross_mark: **${client.user.tag} طلع من سيرفر
+Server name: __${guild.name}__
+Server owner: __${guild.owner}__
+Server id: __${guild.id}__ 
+Server Count: __${guild.memberCount}__**)
+})
 
 
 
