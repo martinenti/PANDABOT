19e6fae2a475fe41b2627f7b51df72e4df6b9a55
martinenti <40662211+martinenti@users.noreply.github.com>
2018-08-22 19:28:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'zg') {
+    if (message.content === 'ping') {
         message.reply('pong');
       }
 });
